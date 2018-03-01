869dbeb5f33c2ebced79e4b2d354389b45fa0803
Pou <poumason@live.com>
2018-03-01 09:52:31 +0800

    support new reqeust / response tages

--- XiaomiAI.SDK/Models/Request/ConextData.cs
+++ XiaomiAI.SDK/Models/Request/ConextData.cs
@@ -9,5 +9,11 @@ namespace XiaomiAI.SDK.Models
         /// </summary>
         [JsonProperty("passport")]
         public object Passport { get; set; }
+
+        /// <summary>
+        /// 表示设备的唯一标识
+        /// </summary>
+        [JsonProperty("device_id")]
+        public string DeviceId { get; set; }
     }
 }
